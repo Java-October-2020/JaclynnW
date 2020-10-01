8ba9813d0d861895b9b7a3ce2a3aaabaf048c958
JACKI WHITSON <jaclynn.whitson@gmail.com>
2020-10-01 15:52:48 -0700

    first commit

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Jaclynn Whitson \n I am 32 years old \n My hometown is Phoenix, AZ");
+    }
+}
